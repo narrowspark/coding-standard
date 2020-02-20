e3defe73bcce3287e04b5ad23d20394157a32f57
prisis <d.bannert@anolilab.de>
2020-02-20 14:15:10 +0100

    feat : updated dependencies and removed tmp solutions

--- bin/CreatePhpCsFixerList.php
+++ bin/CreatePhpCsFixerList.php
@@ -1,5 +1,18 @@
 <?php
+
 declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018-2020 Daniel Bannert
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/narrowspark/php-library-template
+ */
+
+use Narrowspark\CS\Config\Config;
+use Symfony\Component\VarExporter\VarExporter;
 
 final class CreatePhpCsFixerList
 {
@@ -7,20 +20,18 @@ final class CreatePhpCsFixerList
      * Create the php cs fixer rules md file.
      *
      * @throws \Symfony\Component\VarExporter\Exception\ExceptionInterface
-     *
-     * @return int
      */
     public static function build(): int
     {
-        $rules = (new \Narrowspark\CS\Config\Config())->getRules();
+        $rules = (new Config())->getRules();
 
         ksort($rules);
 
-        $content  = '```php';
-        $content .= \Symfony\Component\VarExporter\VarExporter::export($rules);
+        $content = '```php';
+        $content .= VarExporter::export($rules);
         $content .= '```';
 
-        $return = \file_put_contents(\dirname(__DIR__) . DIRECTORY_SEPARATOR . 'PHP-CS-Fixer-Rules-List-PHP'.(\PHP_VERSION_ID >= 70300 ? '7.3.0' : '7.2.0').'.md', $content);
+        $return = file_put_contents(dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'PHP-CS-Fixer-Rules-List.md', $content);
 
         return (int) $return;
     }
