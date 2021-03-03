f2d9077c2a0929362cd1f8ee3568d2f1cc53250c
prisis <d.bannert@anolilab.de>
2021-03-03 10:20:21 +0100

    style: cs fixes

--- bin/CreatePhpCsFixerList.php
+++ bin/CreatePhpCsFixerList.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2018-2020 Daniel Bannert
+ * Copyright (c) 2018-2021 Daniel Bannert
  *
  * For the full copyright and license information, please view
  * the LICENSE.md file that was distributed with this source code.
